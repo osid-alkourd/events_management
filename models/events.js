743947bc224626634beaecdbd36c59e1120e59b0
Osid ALkourd <alkordosid73@gmail.com>
2023-04-29 22:21:34 +0300

    create the events schema

---
+++ models/events.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const eventsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+      },
+    Date: {
+        type: Date,
+        required: true
+    },
+    attendeesNumber: {
+         type: Number,
+         required: true
+    },
+
+    hallId: {
+       type: String , 
+       required: true 
+    } ,
+
+    description: {
+       type: String , 
+       required: true
+    } , 
+
+    startTime: {
+        type: Date , 
+        required: true
+    } , 
+    
+    endTime: {
+        type: Date , 
+        required: true
+    } , 
+
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+      } , 
+
+      updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+      } , 
+})
+
+module.exports = mongoose.model('events',eventsSchema)
