fd5a7f32ccd5b88414814dc10e58ca548e8df073
Osid ALkourd <alkordosid73@gmail.com>
2023-05-02 12:38:54 +0300

    Create the user schema

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+        min: 6,
+        max: 50
+    } ,
+    email: {
+        type: String , 
+        required: true,
+        max:50, 
+        min:10,
+        unique: true
+    } ,
+    password: {
+        type: String,
+        required: true,
+        max: 50 , 
+        min: 6 ,
+    }
+})
